4ddc1f1d5ab1999b9c7f4540c656160eafaa683c
developmentHF <development.henry@gmail.com>
2023-05-30 21:11:55 +1000

    draw window functionality

--- src/golf.c
+++ src/golf.c
@@ -1,17 +1,40 @@
 #include "../lib/raylib.h"
 #include "../lib/raymath.h"
 
-int main(void) { 
-    InitWindow(400, 224, "Template-4.0.0");
+#define FPS (60)
 
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-            ClearBackground(RAYWHITE);
+
+int main(void) { 
+
+    // Initialisations
+    // =========================================================================
+    // Init screen dimensions
+    int screenWidth = 800;
+    int screenHeight = 450;
     
+    // Actually make the window in raylib
+    InitWindow(screenWidth, screenHeight, "golf");
+    SetTargetFPS(FPS);
+    // =========================================================================
+
+    // Game Loop
+    // =========================================================================
+    while(!WindowShouldClose()) {
+        // Updates
+        
+        // Drawing
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+            DrawText("golf", 190, 200, 20, LIGHTGRAY);
+
         EndDrawing();
     }
-    
+    // =========================================================================
+
+    // Cleanup
+    // =========================================================================
     CloseWindow();
-    
+
     return 0;
 }
